ded2caaa4ebc41e6e5aff174f5f0b06547b99b52
pedro <pdohfg@gmail.com>
2025-04-11 23:10:33 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
-console.log("olá jurubira");
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+    const segundosEmMs = 1000;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaEmMs / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / minutoEmMs);
+    const segundosAteOEvento = Math.floor(distanciaAteOEvento % minutoEmMs / segundosEmMs);
+
+    const texto = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    document.getElementById("contador").textContent = texto;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").textContent = 'Evento expirado';
+    }    
+}, 1000);
